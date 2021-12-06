29a3f09c35565f1c2e6bf01ae021c9b193639e47
Usuario <Usuario@RICHARD7>
2021-12-06 15:47:39 -0500

    Inicio
    Clase1: creacion

---
+++ pruebas/src/main/java/pruebas/PracticasEnCasa.java
@@ -0,0 +1,9 @@
+package pruebas;
+
+public class PracticasEnCasa {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ pruebas/src/main/java/pruebas/Tiposdedatos.java
@@ -0,0 +1,9 @@
+package pruebas;
+
+public class Tiposdedatos {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
